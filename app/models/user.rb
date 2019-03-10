7e6131e426b5f05b33cdb1363c62e28ef2e3054c
Jose Medina <fermed28@gmail.com>
2019-03-09 22:24:50 -0500

    add name to users table, and crete validation to create user

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
   has_many :subscriptions
   has_many :computers, :through => :subscriptions
+  validates :name, presence: true
 
   def current_pc
     if (self.subscriptions.count > 0)
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_03_004620) do
+ActiveRecord::Schema.define(version: 2019_03_10_032219) do
 
   create_table "computers", force: :cascade do |t|
     t.string "serial"
@@ -62,6 +62,7 @@ ActiveRecord::Schema.define(version: 2019_03_03_004620) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "enable_loan", default: true
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
