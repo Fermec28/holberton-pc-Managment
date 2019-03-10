447283c3982c830fb6be5a264f9a247818d0ecc6
Jose Medina <fermed28@gmail.com>
2019-03-09 22:45:41 -0500

    add flash messages render in a simple way flash messages Fixes #20 Fixes #22

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -1,29 +1,26 @@
 class SubscriptionsController < ApplicationController
     def index
         if (current_user.current_pc && current_user.current_pc.status != "available")
-            
-
-            p current_user.current_pc.status != :available
-            current_user.current_pc.status
             @mycomputer = current_user.current_pc
         end
         @computers = Computer.disponible
     end
 
     def update
-        registration =  Subscription.find(params[:id])
-        registration.touch
-        registration.computer.update(status: :delivered)
+        subscription =  Subscription.find(params[:id])
+        subscription.touch
+        subscription.computer.update(status: :delivered)
+        flash[:info] = "The chief will check your pc have a nice day!"
         redirect_to :root
     end
 
     def create
         computer = Computer.find(params[:computer_id])
-        registration = computer.subscriptions.new(user_id: current_user.id)
-        if registration.save
-            p "Todo salio bien"
-        else
-            p registration.errors.messages
+        subscription = computer.subscriptions.new(user_id: current_user.id)
+        if subscription.save
+            flash[:success] = ["Take care your PC","Learn a lot"]
+        else             
+            flash[:danger] = subscription.errors.full_messages
         end
 
         redirect_to :root
