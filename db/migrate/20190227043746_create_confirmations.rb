4501ec80c0aee1fd351bfeb2b62bef6af5e8eefd
Jose Medina <fermed28@gmail.com>
2019-03-12 14:37:18 -0500

    change registration for subscription

--- db/migrate/20190227043746_create_confirmations.rb
+++ db/migrate/20190227043746_create_confirmations.rb
@@ -2,7 +2,7 @@ class CreateConfirmations < ActiveRecord::Migration[5.2]
   def change
     create_table :confirmations do |t|
       t.references :user, foreign_key: true
-      t.references :registration, foreign_key: true
+      t.references :subscription, foreign_key: true
 
       t.timestamps
     end
